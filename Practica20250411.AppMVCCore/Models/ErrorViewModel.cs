5951d332eb1dfdc7716d5f42f24e53ee2f71284f
Lupita <lupitastrada1002@gmail.com>
2025-04-11 13:41:00 -0600

    Agregar archivos de proyecto.

---
+++ Practica20250411.AppMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250411.AppMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
